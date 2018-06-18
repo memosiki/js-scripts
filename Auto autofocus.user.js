0bc88441a6426ebf6b91eeae93365ef9f4203352
grenzionky <grenzionky@gmail.com>
2018-06-18 12:48:32 -0400

    Add new script

---
+++ Auto autofocus.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Auto autofocus
+// @version      1.0
+// @description  Auto focuses the cursor in websites to the first input box
+// @author       Abraham Gross
+// @match        *
+// @grant        none
+// @license      MIT License Copyright (c) 2017 Abraham Gross
+// ==/UserScript==
+
+(function() {
+    var inputs = document.getElementsByTagName('input');
+    for(var i=inputs.length-1; i>=0; --i) if(inputs[i].type === "text") inputs[i].focus();
+})();
