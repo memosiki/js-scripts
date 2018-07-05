434d7e83b9a8c60abf322011f56fe6bc9908c2cc
grenzionky <grenzionky@gmail.com>
2018-07-05 13:22:12 -0400

    fixed a whoopsie

--- Auto autofocus.user.js
+++ Auto autofocus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Auto autofocus
-// @version      1.3
+// @version      1.3.1
 // @description  Auto focuses the cursor in websites to the main search field
 // @author       Abraham Gross
 // @include      http*
@@ -15,34 +15,32 @@
     var gotFocused = false;
     //will be used to reiterate over the possible matches to find a more accurate result
     var betterInputs = [];
-	
-	
+
+
 	//first check if the website defined any search fields themselves
 	for(var i=allInputs.length-1; i>=0; --i) {
-		var input = allInputs[i];
-		if(input.type.toLowerCase().startsWith("search")) {
-			input.focus();
+		if(allInputs[i].type.toLowerCase().startsWith("search")) {
+			allInputs[i].focus();
 			gotFocused = true;
 		}
 	}
-	
+
 	//if the website didn't do that, then we have to figure out ourself which field is the search field
 	if(!gotFocused) {
 		//basic algorithm to just isolate text input fields
-		for(var i=allInputs.length-1; i>=0; --i) {
-			var input = allInputs[i];
-			else if(input.type === "text") {
-				input.focus();
-				betterInputs.push(input);
+		for(var j=allInputs.length-1; j>=0; --j) {
+			if(allInputs[j].type === "text") {
+				allInputs[j].focus();
+				betterInputs.push(allInputs[j]);
 				gotFocused = true;
 			}
 		}
 
 		//reiterate over all the text inputs to isolate the best match
-		for(var j = betterInputs.length - 1; j >= 0; --j) {
-			var input = betterInputs[j];
-			if(input.classList.toString().toLowerCase().includes("search") || input.placeholder.toLowerCase().includes("search")) {
-				input.focus();
+		for(var k = betterInputs.length - 1; k >= 0; --k) {
+			var input = betterInputs[k];
+			if(betterInputs[k].classList.toString().toLowerCase().includes("search") || betterInputs[k].placeholder.toLowerCase().includes("search")) {
+				betterInputs[k].focus();
 			}
 		}
 
