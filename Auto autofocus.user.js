8f84217cebabd3fbabe0cbc9c4ab5b136c3d2435
grenzionky <grenzionky@gmail.com>
2018-07-05 12:02:06 -0400

    added better support

--- Auto autofocus.user.js
+++ Auto autofocus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Auto autofocus
-// @version      1.0
+// @version      1.1
 // @description  Auto focuses the cursor in websites to the first input box
 // @author       Abraham Gross
 // @include      http*
@@ -10,5 +10,12 @@
 
 (function() {
     var inputs = document.getElementsByTagName('input');
-    for(var i=inputs.length-1; i>=0; --i) if(inputs[i].type === "text") inputs[i].focus();
+    var gotFocused = false;
+    for(var i=inputs.length-1; i>=0; --i) {
+        if(inputs[i].type === "text") {
+            inputs[i].focus();
+            gotFocused = true;
+        }
+    }
+    if (!gotFocused) inputs[0].focus();
 })();
