45e90ca1d2b6d893f040c6be8945cb74d0aa9e75
grenzionky <grenzionky@gmail.com>
2018-07-05 12:32:37 -0400

    improved search algorithm

--- Auto autofocus.user.js
+++ Auto autofocus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto autofocus
 // @version      1.1
-// @description  Auto focuses the cursor in websites to the first input box
+// @description  Auto focuses the cursor in websites to the first input box or search field
 // @author       Abraham Gross
 // @include      http*
 // @grant        none
@@ -9,13 +9,30 @@
 // ==/UserScript==
 
 (function() {
-    var inputs = document.getElementsByTagName('input');
+    //get all of the inputs in the document
+    var allInputs = document.getElementsByTagName('input');
+    //will be used as a flag if no inputs are found
     var gotFocused = false;
-    for(var i=inputs.length-1; i>=0; --i) {
-        if(inputs[i].type === "text") {
-            inputs[i].focus();
+    //will be used to reiterate over the possible matches to find a more accurate result
+    var betterInputs = [];
+    
+    //basic algorithm to just isolate text input fields
+    for(var i=allInputs.length-1; i>=0; --i) {
+        if(allInputs[i].type === "text") {
+            allInputs[i].focus();
+			betterInputs.push(allInputs[i]);
             gotFocused = true;
         }
     }
-    if (!gotFocused) inputs[0].focus();
+    
+    //reiterate over all the text inputs to isolate the best match
+    for(var i = betterInputs.length - 1; i >= 0; --i) {
+	    var input = betterInputs[i];
+	    if(input.classList.toString().toLowerCase().includes("search") || input.placeholder.toLowerCase().includes("search")) {
+            input.focus();
+        }
+    }
+    
+    //if no text input was found, default to the first input field.
+    if (!gotFocused) allInputs[0].focus();
 })();
