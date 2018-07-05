61b901f964f2ca1f499e72593f34385c00002e85
grenzionky <grenzionky@gmail.com>
2018-07-05 14:21:12 -0400

    even better support

--- Auto autofocus.user.js
+++ Auto autofocus.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         Auto autofocus
-// @version      1.3.1
+// @version      1.4
 // @description  Auto focuses the cursor in websites to the main search field
 // @author       Abraham Gross
 // @include      http*
 // @grant        none
 // @license      MIT
+// @run-at document-idle
 // ==/UserScript==
 
-(function() {
+(function() {window.onload = function() {
+    console.log("script executing");
     //get all of the inputs in the document
     var allInputs = document.getElementsByTagName('input');
     //will be used as a flag if no inputs are found
@@ -17,34 +19,35 @@
     var betterInputs = [];
 
 
-	//first check if the website defined any search fields themselves
-	for(var i=allInputs.length-1; i>=0; --i) {
-		if(allInputs[i].type.toLowerCase().startsWith("search")) {
-			allInputs[i].focus();
-			gotFocused = true;
-		}
-	}
+    //first check if the website defined any search fields themselves
+    for(var i=allInputs.length-1; i>=0; --i) {
+        if(allInputs[i].type.toLowerCase().startsWith("search")) {
+            allInputs[i].focus();
+            gotFocused = true;
+        }
+    }
 
-	//if the website didn't do that, then we have to figure out ourself which field is the search field
-	if(!gotFocused) {
-		//basic algorithm to just isolate text input fields
-		for(var j=allInputs.length-1; j>=0; --j) {
-			if(allInputs[j].type === "text") {
-				allInputs[j].focus();
-				betterInputs.push(allInputs[j]);
-				gotFocused = true;
-			}
-		}
+    //if the website didn't do that, then we have to figure out ourself which field is the search field
+    if(!gotFocused) {
+        //basic algorithm to just isolate text input fields
+        for(var j=allInputs.length-1; j>=0; --j) {
+            if(allInputs[j].type === "text") {
+                allInputs[j].focus();
+                betterInputs.push(allInputs[j]);
+                gotFocused = true;
+            }
+        }
 
-		//reiterate over all the text inputs to isolate the best match
-		for(var k = betterInputs.length - 1; k >= 0; --k) {
-			var input = betterInputs[k];
-			if(betterInputs[k].classList.toString().toLowerCase().includes("search") || betterInputs[k].placeholder.toLowerCase().includes("search")) {
-				betterInputs[k].focus();
-			}
-		}
+        //reiterate over all the text inputs to isolate the best match
+        for(var k = betterInputs.length - 1; k >= 0; --k) {
+            if(betterInputs[k].classList.toString().toLowerCase().includes("search") || betterInputs[k].placeholder.toLowerCase().includes("search")) {
+                betterInputs[k].focus();
+            }
+        }
 
-		//if no text input was found, default to the first input field.
-		if (!gotFocused) allInputs[0].focus();
-	}
-})();
+        //if no text input was found, default to the first input field.
+        if (!gotFocused) allInputs[0].focus();
+    }
+
+    document.activeElement.select();
+}})();
