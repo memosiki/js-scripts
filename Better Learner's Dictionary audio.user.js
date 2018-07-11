c6dc60a3f7f58bd99702b5d99bace3cf66102f0d
grenzionky <grenzionky@gmail.com>
2018-07-11 10:10:00 -0400

    added subaudio support

--- Better Learner's Dictionary audio.user.js
+++ Better Learner's Dictionary audio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Learner's Dictionary audio
 // @namespace    http://tampermonkey.net/
-// @version      7.2.2018
+// @version      7.11.2018
 // @description  Play the word's audio onclick right away without the popup
 // @match        *.learnersdictionary.com/*
 // @author       Abraham Gross
@@ -9,11 +9,13 @@
 // ==/UserScript==
 
 (function() {
-    var soundButton = document.querySelector('[data-file]');
-    soundButton.classList.remove("play_pron");
-    soundButton.onclick = function() {
-        var dir = soundButton.getAttribute('data-dir');
-        var file = soundButton.getAttribute('data-file');
+    var soundButton = document.querySelectorAll('[data-file]');
+
+    soundButton.forEach(function(sound){
+	sound.classList.remove("play_pron");
+    sound.onclick = function() {
+        var dir = sound.getAttribute('data-dir');
+        var file = sound.getAttribute('data-file');
         new Audio("https://media.merriam-webster.com/audio/prons/en/us/mp3/" + dir + "/" + file + ".mp3").play();
-    }
+    }})
 })();
