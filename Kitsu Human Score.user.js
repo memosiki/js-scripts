9650e5119678b3913c2e1d9bbb7999f838db1d9f
grenzionky <grenzionky@gmail.com>
2018-07-15 02:12:10 -0400

    update!

--- Kitsu Human Score.user.js
+++ Kitsu Human Score.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kitsu Human Score
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      7.15.2018 - 1.0
 // @description  Puts words next to the score for a more accurate way to select how you thought an anime was.
 // @author       Abraham Gross
 // @match        https://kitsu.io/users/*
@@ -12,31 +12,50 @@
 
 (function() {
     document.body.onclick = function(){
-        console.log( "Handler for .onclick() called." );
         if(document.querySelector(".noUi-tooltip")){
-            console.log("can read");
+            addWord();
             var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
             var observer = new MutationObserver(function(mutations) {
                 mutations.forEach(function(mutation) {
-                    var score = document.querySelector('div.noUi-tooltip');
-                    console.log(score);
-                    console.log(score.textContent);
-                    switch(score.textContent) {
-                        case "10":score.textContent = "10 (Masterpiece)"; break;
-                        case "9": score.textContent = "9 (Amazing)";      break;
-                        case "8": score.textContent = "8 (Very Good)";    break;
-                        case "7": score.textContent = "7 (Good)";         break;
-                        case "6": score.textContent = "6 (Fine)";         break;
-                        case "5": score.textContent = "5 (Average)";      break;
-                        case "4": score.textContent = "4 (Bad)";          break;
-                        case "3": score.textContent = "3 (Very Bad)";     break;
-                        case "2": score.textContent = "2 (Horrible)";     break;
-                        case "1": score.textContent = "1 (Appaling)";     break;
-                    }
-                    console.log(score.textContent);
+                    addWord();
                 });
             });
             observer.observe(document.querySelector('div.noUi-tooltip'), { childList: true });
         }
     };
 })();
+
+function addWord() {
+    var score = document.querySelector('div.noUi-tooltip');
+    switch(score.textContent) {
+        case "10": score.textContent = score.textContent + " (Masterpiece)";
+            break;
+        case "9.5":
+        case "9": score.textContent = score.textContent + " (Amazing)";
+            break;
+        case "8.5":
+        case "8": score.textContent = score.textContent + " (Very Good)";
+            break;
+        case "7.5":
+        case "7": score.textContent = score.textContent + " (Good)";
+            break;
+        case "6.5":
+        case "6": score.textContent = score.textContent + " (Fine)";
+            break;
+        case "5.5":
+        case "5": score.textContent = score.textContent + " (Average)";
+            break;
+        case "4.5":
+        case "4": score.textContent = score.textContent + " (Bad)";
+            break;
+        case "3.5":
+        case "3": score.textContent = score.textContent + " (Very Bad)";
+            break;
+        case "2.5":
+        case "2": score.textContent = score.textContent + " (Horrible)";
+            break;
+        case "1.5":
+        case "1": score.textContent = score.textContent + " (Appaling)";
+            break;
+    }
+}
